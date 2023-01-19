121edd950932bf4081f851fb5102eb4b649507c1
vagrant <Juelz Lum>
2023-01-19 03:10:57 +0000

    added setup input

--- snake_client/play.js
+++ snake_client/play.js
@@ -2,6 +2,13 @@ const net = require("net");
 const {connect} = require("./client")
 // establishes a connection with the game server
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
