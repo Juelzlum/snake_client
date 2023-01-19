40fafdc50a16062ec532d1755b0d4419578a2778
vagrant <Juelz Lum>
2023-01-19 04:16:34 +0000

    added connections

--- snake_client/input.js
+++ snake_client/input.js
@@ -1,10 +1,22 @@
-const setupInput = function () {
+let connection; 
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
   return stdin;
 };
+const w = "Move: up"
+
+const a = "Move: left"
+
+const s = "Move: down"
+
+const d = "Move: right"
 
 const handleUserInput = function () {
 const stdin = process.stdin
@@ -17,6 +29,19 @@ if(key === '\u0003') {
   stdin.write('Game exit')
   process.exit()
 }
+if(key === w) {
+  interval("Move: up")
+}
+if(key==='s') {
+  interval("Move: down")
+}
+if(key === 'a') {
+  interval("Move: left")
+}
+if(key === 'd'){
+  interval("Move: right")
+}
+
 };
 
 
